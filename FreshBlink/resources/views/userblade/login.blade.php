c6e95120ffd4deecc7b835449806beaf902e7a92
prashiddhajoshi <jprashiddha5@gmail.com>
2025-04-23 20:42:49 +0545

    Minor fix

--- FreshBlink/resources/views/userblade/login.blade.php
+++ FreshBlink/resources/views/userblade/login.blade.php
@@ -3,7 +3,6 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  @vite('resources/css/app.css')
   <title>FreshBlink - Login</title>
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <script src="https://cdn.tailwindcss.com"></script>

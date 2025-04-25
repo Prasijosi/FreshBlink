79274ca464819f6c6a16661b451f34049fb1808d
PrtkPokhrel <pokhrelpratik52@gmail.com>
2025-04-25 13:30:30 +0545

    creates a function to view,approve and reject traders'

---
+++ FreshBlink/app/Http/Controllers/AdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    function index(){
+        
+    }
+    function approve(){
+
+    }
+    function reject(){
+
+    }
+}
--- FreshBlink/routes/web.php
+++ FreshBlink/routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+
 
 Route::get('/', function () {
     return view('userblade.login');
 });
+
+
+//View traders
+Route::get('admin/trader/all', [AdminController::class, 'index']);
+//Approve Traders
+Route::post('admin/traders/{traderId}/approve',[AdminController::class,'approve']);
+//Reject Traders
+Route::post('admin/traders/{traderId}/reject',[AdminController::class,'reject']);
+

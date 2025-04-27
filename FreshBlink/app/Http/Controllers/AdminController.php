cee2a17076a7c7ccd03d319fb2e132daba428192
PrtkPokhrel <pokhrelpratik52@gmail.com>
2025-04-27 12:53:30 +0545

    Admin approval system

--- FreshBlink/app/Http/Controllers/AdminController.php
+++ FreshBlink/app/Http/Controllers/AdminController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     function index(){
-        
+        // $traders=Trader.all();
     }
     function approve(){
 
--- FreshBlink/routes/web.php
+++ FreshBlink/routes/web.php
@@ -16,3 +16,17 @@ Route::post('admin/traders/{traderId}/approve',[AdminController::class,'approve'
 //Reject Traders
 Route::post('admin/traders/{traderId}/reject',[AdminController::class,'reject']);
 
+
+//Admin and trader relationship
+
+
+//Admin panel to view traders
+Route::get('/admin/traders',[AdminController::class,'index']);
+
+//Admin panel to approve traders
+
+Route::post('/admin/traders/{id}/approve',[AdminController::class,'approve']);
+
+//Admin panel to reject traders
+Route::post('/admin/traders/{id}/reject',[AdminController::class,'reject']);
+

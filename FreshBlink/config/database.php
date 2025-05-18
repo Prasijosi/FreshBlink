b1550936fa2681ef4ac546af4ffaf33dbe605bb1
prashiddhajoshi <jprashiddha5@gmail.com>
2025-05-19 00:28:38 +0545

    Oracle Setup

--- FreshBlink/config/database.php
+++ FreshBlink/config/database.php
@@ -30,6 +30,20 @@ return [
     */
 
     'connections' => [
+        'oracle' => [
+                'driver' => 'oracle',
+                'tns' => env('DB_TNS', ''),
+                'host' => env('DB_HOST', '127.0.0.1'),
+                'port' => env('DB_PORT', '1521'),
+                'database' => env('DB_DATABASE', 'XE'),
+                'username' => env('DB_USERNAME', 'FRESHBLINK-DB'),
+                'password' => env('DB_PASSWORD', ''),
+                'charset' => env('DB_CHARSET', 'AL32UTF8'),
+                'prefix' => env('DB_PREFIX', ''),
+                'prefix_schema' => env('DB_SCHEMA_PREFIX', ''),
+                'edition' => env('DB_EDITION', 'ora$base'),
+                'server_version' => env('DB_SERVER_VERSION', '11g'),
+        ],
 
         'sqlite' => [
             'driver' => 'sqlite',

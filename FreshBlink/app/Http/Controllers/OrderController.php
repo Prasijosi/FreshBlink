ee6eceaf0c9ff1a9f2a640d5a741c76224373592
prashiddhajoshi <jprashiddha5@gmail.com>
2025-05-20 21:55:30 +0545

    fix

--- FreshBlink/app/Http/Controllers/OrderController.php
+++ FreshBlink/app/Http/Controllers/OrderController.php
@@ -6,12 +6,12 @@ use App\Models\Cart;
 use App\Models\Order;
 use App\Models\Payment;
 use App\Models\Invoice;
-use App\Models\CustomerController;
+use App\Http\Controllers\CustomerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Str;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\InvoiceEmail;
 
